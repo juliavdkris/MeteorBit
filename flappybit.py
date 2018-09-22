d5143b424d3114bad2478dab00fef7791ca60312
Sam van der Kris <samvdkris@gmail.com>
2018-09-22 21:55:58 +0200

    Clean up some stuff from previous commit

--- flappybit.py
+++ flappybit.py
@@ -23,9 +23,6 @@ class Movable:
 	def move_relative(self, x, y):
 		microbit.display.set_pixel(self.x, self.y, 0)
 		if valid_coords(self.x + x, self.y + y):
-			# self.x += x
-			# self.y += y
-			# microbit.display.set_pixel(self.x, self.y, 9)
 			self.move(self.x, self.y, self.x + x, self.y + y)
 		else:
 			self.outside = True
@@ -38,9 +35,6 @@ class Meteor(Movable):
 	def __init__(self):
 		self.x = randint(0, 4)
 		self.y = 0
-
-	# def __del__(self):
-	# 	meteors.remove(self)
 
 
 class Player(Movable):
